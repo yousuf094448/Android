a3e9b7f74bfc089c24a97eba6d17da69c13816bc
faishal.yousuf <faishal.yousuf@yahoo.com>
2018-05-08 00:45:34 +0600

    Added Home page

--- app/src/main/java/com/chef/app/demo/MainActivity.java
+++ app/src/main/java/com/chef/app/demo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.chef.app.demo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -8,6 +9,9 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        Intent home = new Intent(this, HomeActivity.class);
+        startActivity(home);
+        finish();
     }
 }
