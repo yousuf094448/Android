a2cd723d222f7f4cc5ed81b3be30e7f9e112fef7
faishal.yousuf <faishal.yousuf@yahoo.com>
2018-05-08 22:32:01 +0600

    Added PickUPActivty

--- app/src/main/java/com/chef/app/demo/HomeActivity.java
+++ app/src/main/java/com/chef/app/demo/HomeActivity.java
@@ -1,13 +1,32 @@
 package com.chef.app.demo;
 
-import android.app.Activity;
-import android.os.Bundle;
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.v7.widget.CardView;
+        import android.view.View;
 
 public class HomeActivity extends Activity {
+
+    private CardView pickupView;
+    Intent pickup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        pickup = new  Intent(this, PickUpActivity.class);
+
+        pickupView = findViewById(R.id.pickUp_CardView);
+        pickupView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(pickup);
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/chef/app/demo/PickUpActivity.java
@@ -0,0 +1,14 @@
+package com.chef.app.demo;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PickUpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_up);
+    }
+}
